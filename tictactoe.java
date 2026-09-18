f63fbd8b95a20c90a7a249628d96da4a25730b86
Shagun Rai <raishagun240@gmail.com>
2026-09-18 20:44:56 +0530

    add Calculater.java

---
+++ tictactoe.java
@@ -0,0 +1,26 @@
+public class tictactoe {
+    char[][] board;
+    public tictactoe(){
+       board = new char[3][3];
+       initBoard();
+    }
+    void initBoard(){
+      for(int i= 0; i<board.length; i++){
+        for( int j = 0; j < board[i].length; j++){
+            board[i][j] = ' '; 
+        }
+      }
+       
+    }
+    void displayBoard(){
+        System.out.println("----------------");
+        for(int i= 0; i<board.length; i++){
+            System.out.print("|");
+        for( int j = 0; j <  board[i].length; j++){
+            System.out.print(board[i][j] + " | ");
+            }
+            System.out.println();
+            System.out.println("----------------");
+    }
+    }
+}
